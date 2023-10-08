ab4f3d46ad473404a618e481e508f27bf93ca419
Rexth-ru <annakucher8240@gmail.com>
2023-10-08 18:48:08 +0300

    clinicRepTest

---
+++ src/main/java/org/example/repository/impl/ClinicRepositoryImpl.java
@@ -0,0 +1,122 @@
+package org.example.repository.impl;
+
+import org.example.db.DataSourceConnectHikari;
+import org.example.ex.ModelNotFoundException;
+import org.example.model.Clinic;
+import org.example.model.Doctor;
+import org.example.model.Patient;
+import org.example.repository.ClinicRepository;
+import org.example.repository.mapper.ClinicResultSetMapperImpl;
+import org.example.repository.mapper.DoctorResultSetMapperImpl;
+import org.example.repository.mapper.GeneralResultSetMapper;
+import org.example.repository.mapper.PatientResultSetMapperImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ClinicRepositoryImpl implements ClinicRepository {
+    private GeneralResultSetMapper<Doctor> doctorMapper = new DoctorResultSetMapperImpl();
+    private GeneralResultSetMapper<Patient> patientMapper = new PatientResultSetMapperImpl();
+    private GeneralResultSetMapper<Clinic> clinicMapper = new ClinicResultSetMapperImpl();
+    private DataSourceConnectHikari connect;
+
+    public ClinicRepositoryImpl(DataSourceConnectHikari connect) {
+        this.connect = connect;
+    }
+
+    private final String SQL_SELECT_CLINIC_ID =
+            "SELECT c.clinic_id, c.name_clinic, d.doctor_id, d.name_doctor, d.specialization, p.patient_id, p.name_patient\n" +
+                    "from clinics as c\n" +
+                    "inner join doctors d on d.doctor_id = c.doctor_id\n" +
+                    "inner join doctor_patient dp on c.doctor_id = dp.doctor_id\n" +
+                    "inner join patients p on p.patient_id = dp.patient_id where c.clinic_id = ? order by d.name_doctor";
+    private final String SQL_INSERT_CLINIC =
+            "INSERT INTO clinics (clinic_id, name_clinic) VALUES ((?),(?))";
+    private final String SQL_DELETE_CLINIC_ID =
+            "DELETE FROM clinics WHERE clinic_id =?";
+
+    @Override
+    public Clinic findById(Long id) {
+        try (Connection connection = connect.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_CLINIC_ID)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Clinic clinic = null;
+            if (resultSet.next()) {
+                List<Doctor> doctors = new ArrayList<>();
+                List<Patient> patients = new ArrayList<>();
+                Doctor doctor = doctorMapper.map(resultSet);
+                Patient patient = patientMapper.map(resultSet);
+                clinic = clinicMapper.map(resultSet);
+                patients.add(patient);
+                doctor.setPatients(patients);
+                doctors.add(doctor);
+                while (resultSet.next()) {
+                    if (resultSet.getLong("doctor_id") == doctors.get(doctors.size() - 1).getId()) {
+                        patient = patientMapper.map(resultSet);
+                        patients.add(patient);
+                        doctor.setPatients(patients);
+                    } else {
+
+                        List<Patient> patients1 = new ArrayList<>();
+                        Doctor doctorOther = doctorMapper.map(resultSet);
+                        Patient patient1 = patientMapper.map(resultSet);
+                        patients1.add(patient1);
+                        doctorOther.setPatients(patients1);
+                        doctors.add(doctorOther);
+                    }
+                }
+               clinic.setDoctorList(doctors);
+            }
+         return clinic;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+        @Override
+        public boolean deleteById (Long id) {
+            try (Connection connection = connect.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE_CLINIC_ID)) {
+                preparedStatement.setLong(1, id);
+                int resultSet = preparedStatement.executeUpdate();
+                if (resultSet == 0) throw new ModelNotFoundException();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return true;
+        }
+
+            @Override
+        public Clinic save (Clinic clinic)  {
+
+                try (Connection connection = connect.getConnection();
+                     PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT_CLINIC)) {
+                    preparedStatement.setLong(1, clinic.getId());
+                    preparedStatement.setString(2, clinic.getNameClinic());
+
+                    int resultSet = preparedStatement.executeUpdate();
+                    if (resultSet == 0) throw new ModelNotFoundException();
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                return clinic;
+        }
+
+        @Override
+        public Clinic update (Clinic clinic){
+            Clinic clinicOld = findById(clinic.getId());
+            if (clinicOld != null) {
+                clinicOld.setNameClinic(clinic.getNameClinic());
+                return clinicOld;
+            }
+            throw new RuntimeException();
+        }
+    }
---
+++ src/test/java/org/example/repository/impl/ClinicRepositoryImplTest.java
@@ -0,0 +1,62 @@
+package org.example.repository.impl;
+
+import com.zaxxer.hikari.HikariConfig;
+import org.example.db.DataSourceConnectHikari;
+import org.example.model.Clinic;
+import org.example.model.Doctor;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicRepositoryImplTest {
+    ClinicRepositoryImpl clinicRepository;
+    @Container
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("aston")
+            .withUsername("postgres")
+            .withInitScript("db-migration.sql")
+            .withPassword("469841");
+    @BeforeAll
+    static void setUp() {
+        container.start();
+    }
+    @BeforeEach
+    void setUpHikari(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(container.getJdbcUrl());
+        hikariConfig.setPassword(container.getPassword());
+        hikariConfig.setUsername(container.getUsername());
+        clinicRepository = new ClinicRepositoryImpl(new DataSourceConnectHikari(hikariConfig));
+    }
+    @AfterAll
+    static void stopContainer(){
+        container.stop();
+    }
+    @Test
+    void findById() {
+        Clinic clinic = clinicRepository.findById(1L);
+        Assertions.assertEquals(clinic.getNameClinic(),"Городская поликлиника");
+    }
+
+    @Test
+    void deleteById() {
+        Assertions.assertTrue(clinicRepository.deleteById(2L));
+    }
+
+    @Test
+    void save() {
+        Clinic clinic = new Clinic(5L,"nameClinic");
+        clinicRepository.save(clinic);
+        Assertions.assertEquals(clinic.getNameClinic(),"nameClinic");
+    }
+
+    @Test
+    void update() {
+        Clinic clinic = new Clinic(1L,"nameClinic");
+        clinicRepository.update(clinic);
+        Assertions.assertEquals(clinic.getId(),1L);
+        Assertions.assertEquals(clinic.getNameClinic(),"nameClinic");
+    }
+}
